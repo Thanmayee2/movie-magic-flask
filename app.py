2d021e146295771544f38087877c2695632ce668
Thanmayee2 <bommali.thanmayee25@gmail.com>
2025-07-04 15:10:46 +0530

    Added login functionality

--- app.py
+++ app.py
@@ -174,5 +174,5 @@ def logout():
 
 if __name__ == '__main__':
     print("🚀 Mock MovieMagic running at http://127.0.0.1:5000")
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
